7630f9f6fec000b5336e71e8cf40f218d3b99191
collguy <collguy@larptop.local>
2026-09-13 12:50:16 -0400

    Changes to be committed: 	modified:   flake.nix 	new file:   patches/vfs0090-libfprint-1.94.patch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hyprland on Nixos";
 
   inputs = {
-spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
@@ -17,6 +17,23 @@ spicetify-nix.url = "github:Gerg-L/spicetify-nix";
       system = "x86_64-linux";
 
       modules = [
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              libfprint-2-tod1-vfs0090 =
+                prev.libfprint-2-tod1-vfs0090.overrideAttrs (old: {
+                  patches = (old.patches or []) ++ [
+                    ./patches/vfs0090-libfprint-1.94.patch
+                  ];
+
+                  meta = old.meta // {
+                    broken = false;
+                  };
+                });
+            })
+          ];
+        }
+
         ./configuration.nix
 
         home-manager.nixosModules.home-manager
