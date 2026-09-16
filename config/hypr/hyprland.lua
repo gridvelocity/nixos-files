99dcf9ec03f992e63b85b5ee8789f39d5b54939e
collguy <collguy@larptop.local>
2026-09-15 20:03:10 -0400

    Changes to be committed: 	modified:   config/hypr/hyprland.lua

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -149,7 +149,7 @@ hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURC
 -- Brightness controls
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl set 5%+"))
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl set 5%-"))
-
+hl.bind("XF86PowerOff", hl.dsp.exec_cmd("hyprlock"))
 hl.window_rule({
     match = {
         class = ".*",
